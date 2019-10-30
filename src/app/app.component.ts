2f6b041d20d17acec3714158a60f814298a66801
lijo2 <jonathan_li@wsib.on.ca>
2019-10-30 16:49:24 -0400

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,19 @@
-import { Component, ViewEncapsulation, OnInit } from '@angular/core';
+import { Component, ViewEncapsulation, OnInit, OnDestroy } from '@angular/core';
 import { TabDirective } from 'ngx-bootstrap/tabs';
 import { Router, ActivatedRoute, Params } from '@angular/router';
+import { interval, Subscription } from 'rxjs';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
   // encapsulation: ViewEncapsulation.ShadowDom
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'angularSample';
   currentTab = '';
+  count = 0;
+  private sub: Subscription;
 
   constructor(private router: Router,
               public activatedRoute: ActivatedRoute) {
@@ -21,6 +25,15 @@ export class AppComponent implements OnInit {
     this.activatedRoute.params.subscribe((params: Params) => {
       console.log(params);
     });
+
+    this.sub = interval(1000).subscribe(c  => {
+      this.count = c;
+      console.log(c);
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
   }
 
   onSelect(data: TabDirective): void {
