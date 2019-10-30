22b6b19e88efef631dfe3ce39dc73796a3adf59b
lijo2 <jonathan_li@wsib.on.ca>
2019-10-30 16:25:58 -0400

    update

--- src/directive/basic-highlight.directive.ts
+++ src/directive/basic-highlight.directive.ts
@@ -1,16 +1,28 @@
-import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, OnInit, Renderer2, Input, HostListener } from '@angular/core';
 
 @Directive ({
   selector: '[appBasicHighlight]'
 })
 
 export class BasicHighLightDirective implements OnInit {
+  @Input() colorEnter = 'black';
+  @Input() colorLeave = 'transparent';
+  @HostListener('mouseenter') mouseover(e: Event) {
+    console.log('mouseenter event happene', e);
+    this.render.setStyle(this.elementRef.nativeElement, 'color', this.colorEnter);
+  }
+
+  @HostListener('mouseleave') mouseleave(e: Event) {
+    console.log('mouseenter event happene', e);
+    this.render.setStyle(this.elementRef.nativeElement, 'color', this.colorLeave);
+  }
+
   constructor(private elementRef: ElementRef, private render: Renderer2) {
   }
 
   ngOnInit() {
     this.elementRef.nativeElement.style.backgroundColor = 'green';
-    this.render.setStyle(this.elementRef.nativeElement, 'color', 'white');
+  //  this.render.setStyle(this.elementRef.nativeElement, 'color', 'white');
   }
 
 }
