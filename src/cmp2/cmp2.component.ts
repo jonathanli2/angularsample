48050af937ec2acab3928d799a1caacb5c2f036b
lijo2 <jonathan_li@wsib.on.ca>
2019-11-06 16:36:39 -0500

    update

--- src/cmp2/cmp2.component.ts
+++ src/cmp2/cmp2.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { NgForm, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-cmp2',
@@ -7,10 +8,14 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./cmp2.component.css']
 })
 export class Cmp2Component implements OnInit {
+  // @ViewChild('lre', {static: true})  lastName: NgModel;
 
   constructor(public activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
   }
 
+  onClick(e: NgModel) {
+    console.log(e);
+  }
 }
