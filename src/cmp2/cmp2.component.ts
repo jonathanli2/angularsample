b12ef9f832003bfa04dad76df4a6b153cedf4f08
lijo2 <jonathan_li@wsib.on.ca>
2019-11-07 13:03:15 -0500

    update

--- src/cmp2/cmp2.component.ts
+++ src/cmp2/cmp2.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { NgForm, NgModel } from '@angular/forms';
+import { NgForm, NgModel, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-cmp2',
@@ -9,13 +9,17 @@ import { NgForm, NgModel } from '@angular/forms';
 })
 export class Cmp2Component implements OnInit {
   // @ViewChild('lre', {static: true})  lastName: NgModel;
-
+  myForm: FormGroup;
   constructor(public activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
+    this.myForm = new FormGroup({
+      myFirstName: new FormControl(null, [Validators.required, Validators.minLength(5)])
+    });
   }
 
   onClick(e: NgModel) {
     console.log(e);
+    console.log(this.myForm);
   }
 }
